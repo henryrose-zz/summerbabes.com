8fb736cc8614ed8920e6b4c044f2bb2d0447ffba
Henry Rose <henryrose@gmail.com>
2013-07-26 12:52:02 -0700

    Add press route

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
+    , press = require('./routes/press')
   , http = require('http')
   , path = require('path');
 
@@ -30,6 +31,9 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+
+app.get('/press', press.press_route);
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/press.js
+++ routes/press.js
@@ -2,3 +2,11 @@
  *  GET press page.
  **/
 
+exports.press_route = function (req, res) {
+    res.render(
+        'press',
+        {
+
+        }
+    );
+};
