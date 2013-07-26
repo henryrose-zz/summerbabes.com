2b58acb24901aeb3362a3f3251b91c7bd0737515
Henry Rose <henryrose@gmail.com>
2013-07-25 19:03:35 -0700

    Add some show deets

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,16 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Summer Babes' });
+  res.render(
+      'index',
+      {
+          title: 'Summer Babes',
+          next_gig : {
+              venue : 'Darells',
+              date : 'Friday July 26th',
+              time: '9pm',
+              cover: '$5'
+          }
+      }
+  );
 };
